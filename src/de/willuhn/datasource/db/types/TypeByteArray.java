596bf3d627887b9a25b3131529c16fe661f4ef6d
willuhn <willuhn>
2008-07-11 16:15:54 +0000

    @B rs.getObject() liefert ggf. (abhaengig von der Datenbank) einen InputStream statt byte[]

--- src/de/willuhn/datasource/db/types/TypeByteArray.java
+++ src/de/willuhn/datasource/db/types/TypeByteArray.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/jameica/datasource/src/de/willuhn/datasource/db/types/TypeByteArray.java,v $
- * $Revision: 1.1 $
- * $Date: 2008/07/11 09:30:17 $
+ * $Revision: 1.2 $
+ * $Date: 2008/07/11 16:15:54 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -13,7 +13,11 @@
 
 package de.willuhn.datasource.db.types;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
 
@@ -22,6 +26,47 @@ import java.sql.Types;
  */
 public class TypeByteArray extends TypeGeneric
 {
+  
+  /**
+   * @see de.willuhn.datasource.db.types.TypeGeneric#get(java.sql.ResultSet, java.lang.String)
+   */
+  public Object get(ResultSet rs, String name) throws SQLException
+  {
+    Object value = super.get(rs, name);
+    if (value != null && (value instanceof InputStream))
+    {
+      // Wenn es ein Stream ist, kopieren wir die Daten in ein
+      // Byte-Array
+      InputStream is = (InputStream) value;
+      try
+      {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        byte[] buf = new byte[1024];
+        int len = 0;
+        while ((len = is.read(buf)) != -1)
+          bos.write(buf,0,len);
+        
+        return bos.toByteArray();
+      }
+      catch (IOException ioe)
+      {
+        throw new SQLException("unable to read blob: " + ioe.getMessage());
+      }
+      finally
+      {
+        try
+        {
+          is.close();
+        }
+        catch (IOException e)
+        {
+          throw new SQLException("unable to close inputstream: " + e.getMessage());
+        }
+      }
+    }
+    return value;
+  }
+
   /**
    * @see de.willuhn.datasource.db.types.TypeGeneric#set(java.sql.PreparedStatement, int, java.lang.Object)
    */
@@ -37,6 +82,9 @@ public class TypeByteArray extends TypeGeneric
 
 /*********************************************************************
  * $Log: TypeByteArray.java,v $
+ * Revision 1.2  2008/07/11 16:15:54  willuhn
+ * @B rs.getObject() liefert ggf. (abhaengig von der Datenbank) einen InputStream statt byte[]
+ *
  * Revision 1.1  2008/07/11 09:30:17  willuhn
  * @N Support fuer Byte-Arrays
  * @N SQL-Typen sind jetzt erweiterbar
