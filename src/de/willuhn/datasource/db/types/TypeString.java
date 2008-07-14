d161cd04137665c1f7f1bc9cfb82caed611b4673
willuhn <willuhn>
2008-07-14 12:11:53 +0000

    *** empty log message ***

--- src/de/willuhn/datasource/db/types/TypeString.java
+++ src/de/willuhn/datasource/db/types/TypeString.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/jameica/datasource/src/de/willuhn/datasource/db/types/TypeString.java,v $
- * $Revision: 1.1 $
- * $Date: 2008/07/11 09:30:17 $
+ * $Revision: 1.2 $
+ * $Date: 2008/07/14 12:11:53 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -14,6 +14,7 @@
 package de.willuhn.datasource.db.types;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
 
@@ -22,6 +23,15 @@ import java.sql.Types;
  */
 public class TypeString extends TypeGeneric
 {
+  
+  /**
+   * @see de.willuhn.datasource.db.types.TypeGeneric#get(java.sql.ResultSet, java.lang.String)
+   */
+  public Object get(ResultSet rs, String name) throws SQLException
+  {
+    return rs.getString(name);
+  }
+
   /**
    * @see de.willuhn.datasource.db.types.TypeGeneric#set(java.sql.PreparedStatement, int, java.lang.Object)
    */
@@ -37,6 +47,9 @@ public class TypeString extends TypeGeneric
 
 /*********************************************************************
  * $Log: TypeString.java,v $
+ * Revision 1.2  2008/07/14 12:11:53  willuhn
+ * *** empty log message ***
+ *
  * Revision 1.1  2008/07/11 09:30:17  willuhn
  * @N Support fuer Byte-Arrays
  * @N SQL-Typen sind jetzt erweiterbar
