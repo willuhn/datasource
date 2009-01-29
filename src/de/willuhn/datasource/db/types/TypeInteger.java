93b450753ac63ab3982651bc6fd646cab94ce662
willuhn <willuhn>
2009-01-29 22:31:29 +0000

    @B http://www.onlinebanking-forum.de/phpBB2/viewtopic.php?p=54958#54958

--- src/de/willuhn/datasource/db/types/TypeInteger.java
+++ src/de/willuhn/datasource/db/types/TypeInteger.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/jameica/datasource/src/de/willuhn/datasource/db/types/TypeInteger.java,v $
- * $Revision: 1.1 $
- * $Date: 2008/07/11 09:30:17 $
+ * $Revision: 1.2 $
+ * $Date: 2009/01/29 22:31:29 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -31,13 +31,23 @@ public class TypeInteger extends TypeGeneric
     if (value == null)
       stmt.setNull(index,Types.NULL);
     else
-      stmt.setInt(index,((Integer) value).intValue());
+    {
+      if (value instanceof Integer)
+        stmt.setInt(index,((Integer) value).intValue());
+      else if (value instanceof Long)
+        stmt.setLong(index,((Long) value).longValue());
+      else
+        stmt.setObject(index,value);
+    }
   }
 }
 
 
 /*********************************************************************
  * $Log: TypeInteger.java,v $
+ * Revision 1.2  2009/01/29 22:31:29  willuhn
+ * @B http://www.onlinebanking-forum.de/phpBB2/viewtopic.php?p=54958#54958
+ *
  * Revision 1.1  2008/07/11 09:30:17  willuhn
  * @N Support fuer Byte-Arrays
  * @N SQL-Typen sind jetzt erweiterbar
