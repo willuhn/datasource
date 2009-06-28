bfaff9b47636ebd875f12e03a1a0067a9e3b5640
willuhn <willuhn>
2009-06-28 22:04:09 +0000

    @C java.sql.Blob in Byte-Array kopieren (Heiners Patch)

--- src/de/willuhn/datasource/db/types/TypeByteArray.java
+++ src/de/willuhn/datasource/db/types/TypeByteArray.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/jameica/datasource/src/de/willuhn/datasource/db/types/TypeByteArray.java,v $
- * $Revision: 1.2 $
- * $Date: 2008/07/11 16:15:54 $
+ * $Revision: 1.3 $
+ * $Date: 2009/06/28 22:04:09 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -16,10 +16,13 @@ package de.willuhn.datasource.db.types;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.Blob;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
+
+import de.willuhn.logging.Logger;
 
 /**
  * Typ Byte-Array.
@@ -33,38 +36,53 @@ public class TypeByteArray extends TypeGeneric
   public Object get(ResultSet rs, String name) throws SQLException
   {
     Object value = super.get(rs, name);
-    if (value != null && (value instanceof InputStream))
+    if (value == null)
+      return null;
+
+    InputStream is = null;
+    
+    if (value instanceof InputStream)
     {
-      // Wenn es ein Stream ist, kopieren wir die Daten in ein
-      // Byte-Array
-      InputStream is = (InputStream) value;
+      is = (InputStream) value;
+    }
+    else if (value instanceof Blob)
+    {
+      Blob b = (Blob) value;
+      is = b.getBinaryStream();
+    }
+
+    if (is == null)
+    {
+      Logger.warn("don't know how to handle type " + value.getClass().getName() + " - returning unchanged");
+      return value;
+    }
+    
+    // Daten aus dem InputStream kopieren
+    try
+    {
+      ByteArrayOutputStream bos = new ByteArrayOutputStream();
+      byte[] buf = new byte[1024];
+      int len = 0;
+      while ((len = is.read(buf)) != -1)
+        bos.write(buf,0,len);
+        
+      return bos.toByteArray();
+    }
+    catch (IOException ioe)
+    {
+      throw new SQLException("unable to read blob: " + ioe.getMessage());
+    }
+    finally
+    {
       try
       {
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        byte[] buf = new byte[1024];
-        int len = 0;
-        while ((len = is.read(buf)) != -1)
-          bos.write(buf,0,len);
-        
-        return bos.toByteArray();
+        is.close();
       }
-      catch (IOException ioe)
+      catch (IOException e)
       {
-        throw new SQLException("unable to read blob: " + ioe.getMessage());
-      }
-      finally
-      {
-        try
-        {
-          is.close();
-        }
-        catch (IOException e)
-        {
-          throw new SQLException("unable to close inputstream: " + e.getMessage());
-        }
+        throw new SQLException("unable to close inputstream: " + e.getMessage());
       }
     }
-    return value;
   }
 
   /**
@@ -82,6 +100,9 @@ public class TypeByteArray extends TypeGeneric
 
 /*********************************************************************
  * $Log: TypeByteArray.java,v $
+ * Revision 1.3  2009/06/28 22:04:09  willuhn
+ * @C java.sql.Blob in Byte-Array kopieren (Heiners Patch)
+ *
  * Revision 1.2  2008/07/11 16:15:54  willuhn
  * @B rs.getObject() liefert ggf. (abhaengig von der Datenbank) einen InputStream statt byte[]
  *
